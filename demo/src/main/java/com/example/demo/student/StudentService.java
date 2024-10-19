d9e3765f67d457696889fe04b1ed6802b1d7a61b
ishimwe <ishimwe96@gmail.com>
2024-10-19 15:37:02 +0200

    fix-fixed postgres auth

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -2,9 +2,6 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
